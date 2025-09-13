89d70602e2bbdc3431f27df2114e47e9c5bdb8c0
rzh <rzh@it-gorupp.ru>
2025-09-13 13:44:57 +0300

    add 03 and 04 homework

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "2.2.0"
 }
 
-group = "org.example"
+group = "ru.arab"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,11 +10,12 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
 }
--- src/main/app.kt
+++
@@ -1,4 +0,0 @@
-fun main() {
-    println("Hello World")
-    println("Добро пожаловать в Kotlin!")
-}
